96cdc845ef3fc72dc12ea27afc6ab0efccfe7142
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2026-05-28 12:48:51 -0500

    Documented functions in hal/spinlock.h.

--- include/hal/spinlock.h
+++ include/hal/spinlock.h
@@ -5,8 +5,25 @@
 
 typedef struct spinlock spinlock;
 
-bool spinTryLock(spinlock *);
-void spinLock(spinlock *);
-void spinUnlock(spinlock *);
+/**
+ * @brief Attempt to lock a spinlock.
+ * @note Can spuriously fail.
+ * @param sp The spinlock to lock.
+ */
+bool spinTryLock(spinlock *sp);
+
+/**
+ * @brief Wait on and lock a spinlock.
+ * @param sp The spinlock to lock.
+ */
+void spinLock(spinlock *sp);
+
+/**
+ * @brief Unlock a spinlock.
+ * @note The spinlock must have previously been
+ * successfully locked or the behavior is undefined.
+ * @param sp The spinlock to unlock.
+ */
+void spinUnlock(spinlock *sp);
 
 #endif
